73b3885008d8d751ff5486ce1542dd3496eec4ad
baaki20 <huduabdulbaakijunior20@gmail.com>
2023-07-10 14:39:30 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,13 +20,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open("Requiescat", O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * (letters + 1));
 	if (buffer == NULL)
 	{
+		close(fd);
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
